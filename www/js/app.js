dd45d42bdd72ab383722a358bc5c34c889a0fea9
Swarn Singh <swarn1120@gmail.com>
2018-04-13 13:56:45 -0500

    final fixes here adn there

--- www/js/app.js
+++ www/js/app.js
@@ -168,8 +168,10 @@ function gotData(data) {
       lat: latitude,
       lng: longitude
     };
-    var navigation = 'https://www.google.com/maps/place/' + location;
-    var contentString = '<div class="animated fadeInUp"><div class="card" id="rcorners2"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + image + '" id="img"><div class="top-left"><h6>' + category + '</h6></div><div class="top-left2"><h5>' + eventName + '</h5></div><div class="bottom-left">'+date +'</div><div class="bottom-right">'+startTime+' | '+endTime+'</div></div><div class="card-reveal"><span class="card-title grey-text text-darken-4"><b>'+eventName+'</b><i class="material-icons right activator">close</i></span><br><p>' + description + '</p><br><div class="row center-align"><div class="col s6"><a class="button is-medium blue-text grey lighten-3" id="rcorners3" href="'+info+'" target="_blank">More Info</a></div><div class="col s6"><a class="button is-medium white-text blue" id="rcorners3"                  href="'+navigation+ '">Directions</a></div></div></div></div></div>';
+    var navLink = 'https://www.google.com/maps/place/' + location;
+    var link = "window.open('"+info+"', '_blank');";
+    var navigation = "window.open('"+navLink+"', '_blank');";
+    var contentString = '<div class="animated fadeInUp"><div class="card" id="rcorners2"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + image + '" id="img"><div class="top-left"><h6>' + category + '</h6></div><div class="top-left2"><h5>' + eventName + '</h5></div><div class="bottom-left">'+date +'</div><div class="bottom-right">'+startTime+' | '+endTime+'</div></div><div class="card-reveal"><span class="card-title grey-text text-darken-4"><b>'+eventName+'</b><i class="material-icons right activator">close</i></span><br><p>' + description + '</p><br><div class="row center-align"><div class="col s6"><a class="button is-medium blue-text grey lighten-3" id="rcorners3"  onclick="'+link+'">More Info</a></div><div class="col s6"><a class="button is-medium white-text blue" id="rcorners3"      onclick="'+navigation+'">Directions</a></div></div></div></div></div>';
     totalContentString.push(contentString);
 
     for (var j = 0; j < totalContentString.length; j++) {
